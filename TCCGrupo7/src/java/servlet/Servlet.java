345e63b0649fc217e8b6ee57db76bcd6c0fdf74a
andre ulisses <andre ulisses@Notebook-SLS>
2015-10-05 16:06:51 -0300

    Projeto atualizado

--- TCCGrupo7/src/java/servlet/Servlet.java
+++ TCCGrupo7/src/java/servlet/Servlet.java
@@ -1,10 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package servlet;
 
+import facade.UsuarioFacade;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -12,36 +8,33 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author andre ulisses
- */
 public class Servlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet Servlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet Servlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+
+        if (request.getParameter("txtObjeto").equals("Usuario")) {
+            
+            UsuarioFacade usuarioFacade = new UsuarioFacade();
+            switch (request.getParameter("txtMetodo")) {
+                case "Cadastrar":
+                    usuarioFacade.incluir(request, response);
+                    break;
+                case "Salvar":
+                    usuarioFacade.salvar(request, response);
+                    break;
+                case "Editar":
+                    usuarioFacade.editar(request, response);
+                    break;
+                case "Listar":
+                    usuarioFacade.listar(request, response);
+                    break;
+                case "Excluir":
+                    usuarioFacade.excluir(request, response);
+                    break;
+            }
+
+        }/* fim do objeto Usuario*/
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
